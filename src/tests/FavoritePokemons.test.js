6eddf44ad3a418bfd2d18d2cb3cf815058c4df58
Alan Foster <alan-af@outlook.com>
2022-09-19 15:22:26 -0300

    3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+describe('Teste se são exibidos todos os cards de pokémons favoritados', () => {
+  it('Teste se é exibida a mensagem No favorite pokemon found', () => {
+    render(<FavoritePokemons />);
+
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
