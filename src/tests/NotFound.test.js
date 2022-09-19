7cb360c90409ab9a19f54e9e8f109ab75a9dfa59
Alan Foster <alan-af@outlook.com>
2022-09-19 15:45:53 -0300

    4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen, render } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+
+describe('Test NotFound', () => {
+  it('A página contém um heading h2 com o texto Page requested not found', () => {
+    render(<NotFound />);
+
+    const notFound = screen
+      .getByRole('heading', { level: 2 });
+    expect(notFound).toHaveTextContent('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('Se renderiza a imagem', () => {
+    render(<NotFound />);
+
+    const img = screen
+      .getByRole(
+        'img',
+        { name: 'Pikachu crying because the page requested was not found' },
+      );
+    expect(img.src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
